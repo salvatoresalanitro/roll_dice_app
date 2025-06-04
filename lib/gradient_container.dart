59e72418179c65480bb334dcd3b854be5c3a7aae
Salvo <salvatoresalanitro94@gmail.com>
2025-06-04 16:52:40 +0200

    fix styled_text

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
+const Alignment startAlignment = Alignment.topLeft;
+const Alignment endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            const Color.fromARGB(199, 213, 16, 16),
-            const Color.fromARGB(226, 233, 102, 102),
+            Color.fromARGB(199, 213, 16, 16),
+            Color.fromARGB(226, 233, 102, 102),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
+          begin: startAlignment,
+          end: endAlignment
           ),
       ),
-      child: Center(
+      child: const Center(
         child: StyledText(),
       ),
     );
