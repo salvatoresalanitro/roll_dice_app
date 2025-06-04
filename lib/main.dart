16ad5c8560cb051036193c9f902de5bf6569eb46
Salvo <salvatoresalanitro94@gmail.com>
2025-06-04 16:11:02 +0200

    create first scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(199, 213, 16, 16),
+                const Color.fromARGB(226, 233, 102, 102),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+              ),
+          ),
+          child: Center(child: Text('Welcome!')),
+        ),
+      ),
+    ),
+  );
 }
