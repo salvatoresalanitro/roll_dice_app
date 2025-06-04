45921105106a191775401643220249de2be1b8a6
Salvo <salvatoresalanitro94@gmail.com>
2025-06-04 17:00:52 +0200

    add paramether to class to reuse styleText

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.outputText, {super.key});
+
+  final String outputText;
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      'Welcome!',
+      outputText,
       style: TextStyle(
         color: Colors.white,
         fontSize: 28,
