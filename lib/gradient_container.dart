1f42fa70fa73f86d3746f508515b80dc096308f8
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 08:43:00 +0200

    add function do roll dice and style home

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,10 +4,14 @@ const Alignment startAlignment = Alignment.topLeft;
 const Alignment endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key, required this.gradientColors});
-  
+  const GradientContainer({
+    super.key,
+    required this.gradientColors,
+  });
 
   final List<Color> gradientColors;
+
+  void _rollDice() {}
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +20,30 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
           colors: gradientColors,
           begin: startAlignment,
-          end: endAlignment
-          ),
+          end: endAlignment,
+        ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-images/dice-1.png',
-          width: 200,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-images/dice-1.png',
+              width: 200,
+            ),
+            SizedBox(height: 20,),
+            TextButton(
+              onPressed: _rollDice,
+              style: TextButton.styleFrom(
+                // padding: EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: Text('Roll Dice'),
+            ),
+          ],
         ),
       ),
     );
